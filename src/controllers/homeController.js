63b0edeafec1ef8169d9f2cc6e4bfae35a946b3b
Paundra <h1101241029@student.untan.ac.id>
2026-04-02 23:40:38 +0000

    progres 1.4

--- src/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-import { render } from "../config/viewEngine";
-
-export const home = async (c) => {
-  const html = await render("home", {
-    title: "Dashboard Bun MVC",
-    message: "Hello dari Bun + Tailwind"
-  }, c);
-  return c.html(html);
-};
